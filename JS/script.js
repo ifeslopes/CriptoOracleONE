b6bc5a6074315507aa529bf78bc6048831de6012
Lopes <ifes.lopes@gmail.com>
2022-01-24 21:48:47 -0300

    finalizando

--- JS/script.js
+++ JS/script.js
@@ -23,11 +23,28 @@ var nome;
 botaocript.addEventListener("click", function(event) {
   event.preventDefault();
   var textoNormal = document.querySelector("#input-texto");
-  alert(textoNormal.value);
+
+  var erro = document.querySelector(".erro");
+
   nome = textoNormal.value;
-  var textoCripto = document.querySelector("#msg");
-  textoCripto.value = criptoDescripto(nome);
-  textoNormal.value = "";
+  if (!nome) {
+    // verificar se caixa texto esta vazia
+
+    erro.textContent = "insira um valor válido!"
+  } else {
+    var teste = remover_acentos_espaco(nome)
+
+    if (teste) {
+      var textoCripto = document.querySelector("#msg");
+      textoCripto.value = criptoDescripto(nome);
+      textoNormal.value = "";
+
+      erro.textContent = "";
+    } else {
+
+      erro.textContent = "Erro! Sistema só é aceito letrassem acento e minúsculo";
+    }
+  }
 });
 
 var botaodescript = document.querySelector("#btn-descripto")
@@ -37,17 +54,14 @@ botaodescript.addEventListener("click", function (event) {
   var nome = textoCripto.value;
   textoCripto.value = "";
   textoCripto.value = descripiti(nome);
-
-
-
 })
 
 
 
 //Função sem replace
 function criptoDescripto(palavra) {
-  var  vogais = ["a","e","i", "o","u"];
-  var  cript = ["ai","enter", "imes","ober","ufat"];
+  var  vogais = ["a", "e", "i", "o", "u"];
+  var  cript = ["ai", "enter", "imes", "ober", "ufat"];
   var nome = vogais.indexOf(palavra.charAt(0));
 
   var nomecrip = '';
@@ -62,21 +76,36 @@ function criptoDescripto(palavra) {
 }
 //Função com replaceAll
 function descripiti(palavra) {
-  var  vogais = ["a","e","i","o","u"];
+  var  vogais = ["a", "e", "i","o","u"];
   var  cript = ["ai","enter","imes","ober","ufat"];
   var nomecrip = palavra;
   for (var i = 0; i < cript.length+1; i++) {
     nomecrip = nomecrip.replaceAll(cript[i], vogais[i])
 
   }
-  alert(nomecrip);
+
   return nomecrip;
 }
 
 //função copiar
 var botaoCopia = document.querySelector('#btn-copy');
 botaoCopia.addEventListener('click', execCopy);
-function execCopy() {
+function execCopy(event) {
+  event.preventDefault();
   document.querySelector("#msg").select();
   document.execCommand("copy");
+}
+
+//testar se tem maiúsculas números com regex
+function remover_acentos_espaco(str) {
+  const regex = /[a-z ]{1,}/
+
+  for (var i = 0; i < str.length; i++) {
+    var teste = str.charAt(i);
+    if (!regex.test(teste)) {
+      return false;
+    }
+  }
+
+  return true;
 }
